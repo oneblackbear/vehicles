811a1aa4b691bfa97adc2e2e28605cdb7d122c98
charles marshall <charles@oneblackbear.com>
2012-03-17 22:08:25 +0000

    media joins are good

--- lib/model/Model.php
+++ lib/model/Model.php
@@ -4,7 +4,7 @@ class Model extends VehicleBaseModel{
   public function setup(){
     parent::setup();
     //remove categories & media
-    unset($this->columns['categories'], $this->columns['media']);
+    unset($this->columns['categories']);
     $this->define("brands", "ManyToManyField", array('target_model'=>'Brand', 'group'=>'relationships'));
     $this->define("derivatives", "ManyToManyField", array('target_model'=>'Derivative', 'group'=>'relationships'));
   }
