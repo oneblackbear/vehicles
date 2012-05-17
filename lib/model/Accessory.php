ddbb5a0d31d2a3625fe2ab524c5351d752bd264a
charles marshall <charles@oneblackbear.com>
2012-05-17 16:41:54 +0200

    change the fitting times to a drop down

--- lib/model/Accessory.php
+++ lib/model/Accessory.php
@@ -1,5 +1,6 @@
 <?
 class Accessory extends VehicleBaseModel{
+  public static $times = array('0.25'=>0.25, '0.5'=>0.5, '0.75'=>0.75, 1=>'1.00', '1.25'=>'1.25', '1.50'=>'1.50', '1.75'=>'1.75', '2.00'=>2, '2.25'=>'2.25', '2.5'=>'2.5', '2.75'=>'2.75', '3'=>'3.00');
   public $unset_from_top = array('Brand'=>array('derivatives'), 'Model'=>array('derivatives'), 'Derivative'=>array('derivatives'));
   public function setup(){
     parent::setup();
@@ -8,8 +9,7 @@ class Accessory extends VehicleBaseModel{
     $this->define("dealer_price", 'CharField');
     $this->define("retail_price", 'CharField', array('scaffold'=>true)); //excluding VAT
 
-    $this->define("fitting_time", 'TextField');
-    $this->define("fitting_instructions", 'TextField');
+    $this->define("fitting_time", 'CharField', array('widget'=>'SelectInput', 'choices'=>self::$times));
     $this->define("components", 'TextField');
 
     $this->define("derivatives", "ManyToManyField", array('target_model'=>'Derivative', 'group'=>'relationships', 'scaffold'=>true));
