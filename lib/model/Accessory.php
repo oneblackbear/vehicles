b92a4785c3f91a63c7cb661879b1e6bc6851def6
charles marshall <charles@oneblackbear.com>
2012-05-17 17:02:44 +0200

    tweak time drop down

--- lib/model/Accessory.php
+++ lib/model/Accessory.php
@@ -1,6 +1,6 @@
 <?
 class Accessory extends VehicleBaseModel{
-  public static $times = array(''=>'-- select --','0.25'=>0.25, '0.5'=>0.5, '0.75'=>0.75, 1=>'1.00', '1.25'=>'1.25', '1.50'=>'1.50', '1.75'=>'1.75', '2.00'=>2, '2.25'=>'2.25', '2.5'=>'2.5', '2.75'=>'2.75', '3'=>'3.00');
+  public static $times = array(''=>'-- select --','0.25'=>0.25, '0.50'=>0.5, '0.75'=>0.75, 1=>'1.00', '1.25'=>'1.25', '1.50'=>'1.50', '1.75'=>'1.75', '2.00'=>2.00, '2.25'=>'2.25', '2.50'=>'2.50', '2.75'=>'2.75', '3.00'=>'3.00');
   public $unset_from_top = array('Brand'=>array('derivatives'), 'Model'=>array('derivatives'), 'Derivative'=>array('derivatives'));
   public function setup(){
     parent::setup();
