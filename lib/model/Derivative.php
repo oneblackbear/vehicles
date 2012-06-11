1f0dbbc86271090c6fb9bf97ee7e9bf2c1baaff7
Ross Riley <riley.ross@gmail.com>
2012-06-11 15:55:27 +0100

    urls for models / derivatives

--- lib/model/Derivative.php
+++ lib/model/Derivative.php
@@ -5,6 +5,8 @@ class Derivative extends VehicleBaseModel{
   public function setup(){
     parent::setup();
     $this->define("model_title", "CharField");
+    $this->define("url", "CharField", array('editable'=>false));
+    
     //link to the model
     $this->define("models", "ManyToManyField", array('target_model'=>'Model', 'group'=>'relationships', 'scaffold'=>true));
     $this->define("details", "ManyToManyField", array('target_model'=>'InformationGroup', 'group'=>'relationships'));
@@ -17,8 +19,13 @@ class Derivative extends VehicleBaseModel{
     if(!$this->model_title && $this->title){
       if(($models = $this->models) && ($model = $models->first())) $this->model_title .= $model->title." ".$this->title;
     }
+    if(!$this->url) $this->url = Inflections::to_url($this->title);;
+    
 
   }
+  
+  
+  
 
 }
 ?>
--- lib/model/Model.php
+++ lib/model/Model.php
@@ -5,9 +5,16 @@ class Model extends VehicleBaseModel{
     parent::setup();
     //remove categories & media
     unset($this->columns['categories']);
+    $this->define("url", "CharField", array('editable'=>false));
+    
     $this->define("brands", "ManyToManyField", array('target_model'=>'Brand', 'group'=>'relationships'));
     $this->define("derivatives", "ManyToManyField", array('target_model'=>'Derivative', 'group'=>'relationships'));
   }
+  
+  public function before_save(){
+    parent::before_save();
+    if(!$this->url) $this->url = Inflections::to_url($this->title);;
+  }
+  
 
 }
-?>
